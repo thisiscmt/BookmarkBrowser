0d21328c18dfd70558b319a1ade5ef6dfeffa964
thisiscmt <thisiscmt@gmail.com>
2016-03-27 23:26:56 -0700

    File cleanup

---
+++ BookmarkBrowser/gruntfile.js
@@ -0,0 +1,52 @@
+﻿var pkgJson = require('./package.json');
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        // Concat the specified files into mb-core.js
+        concat: {
+            bmb_core : {
+                src: [
+                    'scripts/bmb/bookmark-browser-common.js',
+                    'scripts/bmb/bookmark-browser-default.js',
+                    'scripts/view-models/bookmarks-view-model.js',
+                ],
+                dest: 'scripts/bmb-core.js'
+            }
+        },
+        // Minify the combined JS file
+        uglify: {
+            options: {
+                // Banner for inserting at the top of the result
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy hh:") %> */\n',
+                sourceMap: true,
+                sourceMapName: 'scripts/sourceMap.map'
+            },
+            bmb_core: {
+                src: [
+                    'Scripts/bmb-core.js'
+                ],
+                dest: 'Scripts/bmb-core.min.js'
+            }
+        },
+        cachebreaker: {
+            bmb_core: {
+                options: {
+                    match: ['scripts/bmb-core.min.js'],
+                    replacement: function (){
+                        return pkgJson.version;
+                    }
+                },
+                files: {
+                    src: ['index.html']
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-cache-breaker');
+
+    grunt.registerTask('default', ['concat:bmb_core', 'uglify:bmb_core', 'cachebreaker:bmb_core']);
+}
