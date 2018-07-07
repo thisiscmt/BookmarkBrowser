0dd7fcc03e94923af5b2e5aa9be6361a58a76588
Chris Tybur <thisiscmt@gmail.com>
2018-07-07 14:21:24 -0700

    Increased space around header buttons

---
+++ BookmarkBrowserUI/src/directives/bmbBookmark/bmbBookmark.js
@@ -0,0 +1,18 @@
+﻿angular.module('bookmarkBrowser.directives.bmbBookmark', [
+]).directive('bmbBookmark', function ($state) {
+    return {
+        restrict: 'E',
+        templateUrl: 'directives/bmbBookmark/bmbBookmark.tpl.html',
+        scope: {
+            bookmark: '='
+        },
+        link: function ($scope, element, attrs) {
+            $scope.directoryImage = "images/folder.png";
+            $scope.bookmarkImage = "images/bookmark.png";
+
+            $scope.goToDirectory = function goToDirectory(bookmark) {
+                $scope.$emit("BMB_GoToDirectory", { bookmark: bookmark });
+            };
+        }
+    };
+});
